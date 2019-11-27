1133d742ee301ea6636a180336568d07dbe953fe
João Bosco <joaobosconff03@gmail.com>
2019-11-27 09:00:33 -0300

    implementado eureka,zuul e 2 microservices

---
+++ users/src/test/java/br/unifacisa/map/users/UsersApplicationTests.java
@@ -0,0 +1,13 @@
+package br.unifacisa.map.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ zuul/src/main/java/br/unifacisa/map/zuul/ZuulApplication.java
@@ -0,0 +1,24 @@
+package br.unifacisa.map.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+public class ZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulApplication.class, args);
+	}
+	
+	
+	@Bean
+	public SimpleFilter simpleFilter() {
+		return new SimpleFilter();
+	}
+
+}
